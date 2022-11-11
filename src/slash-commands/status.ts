c30674b78f6a501c35d7d2028a9bccef7ce03370
Androz2091 <androz2091@gmail.com>
2022-11-11 12:56:35 +0100

    fix formatting in status

--- src/slash-commands/status.ts
+++ src/slash-commands/status.ts
@@ -50,10 +50,9 @@ export const run: SlashCommandRunFunction = async (interaction) => {
         .addFields([
             {
                 name: 'Subscriptions',
-                value: subscriptions.length > 0 ? subscriptions.map((subscription: any) => ({
-                    name: subscription.items.data[0]?.plan.interval,
-                    value: subscription.status === 'active' ? "✅ Active" : ((subscription.cancel_at && subscription.current_period_end > Date.now() / 1000) ? "❌ Cancelled (not expired yet)" : "❌ Cancelled")
-                })).join('\n') : "There is no subscription for this account."
+                value: subscriptions.length > 0 ? subscriptions.map((subscription: any) => {
+                    return `${subscription.items.data[0]?.plan.interval} (${subscription.status === 'active' ? "✅ Active" : ((subscription.cancel_at && subscription.current_period_end > Date.now() / 1000) ? "❌ Cancelled (not expired yet)" : "❌ Cancelled")})`
+                }).join('\n') : "There is no subscription for this account."
             },
             {
                 name: 'Lifetime',
