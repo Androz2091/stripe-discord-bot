211a0e6419d92c41cae41db2e332f5a744a2b3d9
Androz2091 <androz2091@gmail.com>
2023-02-10 16:40:14 +0100

    fix

--- load.js
+++ load.js
@@ -1,7 +1,20 @@
+const { config } = require('dotenv');
+config();
+
 const fs = require('fs');
 
 const csvPath = './data.csv';
 const csv = fs.readFileSync(csvPath, 'utf8');
+
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    intents: [
+        Discord.IntentsBitField.Flags.GuildMembers,
+        Discord.IntentsBitField.Flags.Guilds,
+    ]
+});
+
+client.login(process.env.DISCORD_CLIENT_TOKEN);
 
 const papa = require('papaparse');
 
@@ -11,13 +24,29 @@ const data = papa.parse(csv, {
     skipEmptyLines: true
 });
 
-const sqlQuery = `INSERT INTO discord_customer ("discordUserId", "email", "hadActiveSubscription") VALUES `
-const values = [];
+const members = [];
 
-for (let row of data.data) {
-    values.push(`('${row.id}', '${row.email}', true)`);
-}
+client.on('ready', async () => {
 
-const sql = sqlQuery + values.join(', ');
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    await guild.members.fetch();
 
-fs.writeFileSync('./dump.sql', sql);
+    const sqlQuery = `INSERT INTO discord_customer ("discordUserId", "email", "hadActiveSubscription") VALUES `
+    const values = [];
+
+    console.log(data.data)
+    
+    for (let row of data.data) {
+        const member = guild.members.cache.find((m) => m.user.tag === row['Discord name with #']);
+        if (!member) {
+            members.push(row['Discord name with #'])
+        } else values.push(`('${member.id}', '${row['Buyer E-mail']}', true)`);
+    }
+    
+    const sql = sqlQuery + values.join(',\n');
+    
+    fs.writeFileSync('./dump.sql', sql);
+    fs.writeFileSync('./members.json', JSON.stringify(members, null, 2));
+
+});
+
--- src/slash-commands/status.ts
+++ src/slash-commands/status.ts
@@ -58,7 +58,10 @@ export const run: SlashCommandRunFunction = async (interaction) => {
             {
                 name: 'Subscriptions',
                 value: subscriptions.length > 0 ? subscriptions.map((subscription: any) => {
-                    return `${subscription.items.data[0]?.plan.nickname} (${subscription.status === 'active' ? "✅ Active" : ((subscription.cancel_at && subscription.current_period_end > Date.now() / 1000) ? "❌ Cancelled (not expired yet)" : "❌ Cancelled")})`
+                    const name = subscription.items.data[0]?.plan.id
+                    .replace(/_/g, ' ')
+                    .replace(/^\w|\s\w/g, (l: string) => l.toUpperCase())
+                    return `${name} (${subscription.status === 'active' ? "✅ Active" : ((subscription.cancel_at && subscription.current_period_end > Date.now() / 1000) ? "❌ Cancelled (not expired yet)" : "❌ Cancelled")})`
                 }).join('\n') : "There is no subscription for this account."
             },
             {
